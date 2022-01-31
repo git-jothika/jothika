6b8718c215d0288028557dda59b0615a873ab30f
ELCOT <ELCOT@Lenovo>
2022-01-31 15:59:03 +0530

    2nd commit

---
+++ Ascii/src/asciivalue.java
@@ -0,0 +1,11 @@
+
+public class asciivalue {
+ public static void main(String[] args)
+ {
+	 char ch='E';
+	 int ascii=ch;
+	 int castascii=(int)ch;
+	 System.out.println("The Ascii value of" +ch + " is"+ ascii);
+	 System.out.println("The Ascii value of" +ch+" is"+ castascii);
+ }
+}
