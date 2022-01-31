8a713dd4d854d0e9c95473a94cb848e3669d0878
ELCOT <ELCOT@Lenovo>
2022-01-31 16:01:29 +0530

    4th

---
+++ dowhile/src/dowhile/do_while.java
@@ -0,0 +1,15 @@
+package dowhile;
+import java.util.Scanner;
+public class do_while {
+	public static void main(String[] args) {
+		int x=1;
+		do
+		{
+		System.out.print("value of X" + x);
+		x++;
+		System.out.print("\n");
+		}while(x<11);
+	}
+	                                      
+
+}
