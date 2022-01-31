b40d9fe78a74f72c44ce6b6f958365473301378b
ELCOT <ELCOT@Lenovo>
2022-01-31 15:55:29 +0530

    my first commit

---
+++ addfin/src/addfin/addition.java
@@ -0,0 +1,14 @@
+package addfin;
+
+public class addition {
+	public static void main(String[] argS)
+	{
+		System.out.println("Enter Two Numbers");
+		int first=2;
+		int second=3;
+		System.out.println(first+" "+second);
+		int sum=first+second;
+		System.out.println("The Sum is:" +sum);
+	}
+
+}
